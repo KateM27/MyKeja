a25f8a89035f250d9652951302d0bb9980d06a2e
Trevor <chelimotrevorkibowen@gmail.com>
2023-02-09 02:21:52 +0300

    relevant pages are now working.

--- frontend/auth.py
+++ frontend/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p>login</p>"
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"
+    return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/signup')
 def sign_up():
-    return "<p>sign up</p>"
+    return render_template('signup.html')
