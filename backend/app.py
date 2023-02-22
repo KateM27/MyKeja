0dc74ad6cecf3e974cc0638c87aaf6c2a52a8e9f
KateM27 <mamocatherine@gmail.com>
2023-02-22 09:54:07 +0300

    update db

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, redirect
-from flask.templating import render_template
+from flask import Flask, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 # from routes import *
 
 app = Flask(__name__)
