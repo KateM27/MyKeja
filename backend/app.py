f975b9577a45462970cda4326c716bc2cfd3d792
KateM27 <mamocatherine@gmail.com>
2023-02-22 09:57:14 +0300

    update

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, redirect
-from flask.templating import render_template
+from flask import Flask, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, migrate
+
+# from routes import *
 
 app = Flask(__name__)
 
@@ -11,43 +11,37 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mykeja.db'
 with app.app_context():
 	db = SQLAlchemy(app)
 
-migrate = Migrate(app, db)
-
 # Models
 class Tenant(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(20), unique=False, nullable=False)
-	email = db.Column(db.String(64), unique=False, nullable=False)
+	ID_No = db.Column(db.Integer, unique=False, nullable=False)
 	phone = db.Column(db.Integer, nullable=False)
 
 	def __repr__(self):
 		return f"Name : {self.name}, Phone: {self.phone}"
 
-@app.route('/')
-def home():
-    tenants = Tenant.query.all()
-    return render_template('home.html', tenants=tenants)
-
-@app.route('/add_tenant')
-def new_tenant():
-    return render_template('add_tenant.html')
-
 # add tenants to db
-@app.route('/add', methods=["POST"])
+@app.route('/add_tenant', methods=["POST"])
 def profile():
 	name = request.form.get("name")
-	email = request.form.get("email")
+	ID_No = request.form.get("ID_No")
 	phone = request.form.get("phone")
 
 	# create an object of the Profile class of models
 	# and store data as a row in our datatable
-	if name != '' and email != '' and phone is not None:
-		t = Tenant(name=name, email=email, phone=phone)
+	if name != '' and ID_No != '' and phone is not None:
+		t = Tenant(name=name, ID_No=ID_No, phone=phone)
 		db.session.add(t)
 		db.session.commit()
 		return redirect('/')
 	else:
 		return redirect('/')
+
+@app.route('/', methods=['GET'])
+def all_tenants():
+	view_tenants = Tenant.query.all()
+	return render_template('home.html', tenants=view_tenants)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -55,6 +49,18 @@ def delete(id):
 	db.session.delete(data)
 	db.session.commit()
 	return redirect('/')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/add_tenant')
+def add_tenant():
+    return render_template('add_tenant.html')
+
+@app.route('/delete/<int:id>')
+def delete_tenant(id):
+    return 'Delete Tenant'
 
 
 if __name__ == "__main__":
